62c31ad987f30d0c6848f3450b71fff8dc6bd090
JayPalkar <jaypalkar654@gmail.com>
2024-03-16 18:44:51 +0530

    Research Paper Form Complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React from 'react' // eslint-disable-line no-unused-vars
 import Footer from './component/headerFooter/Footer';
 import Header from './component/headerFooter/Header';
 import TimeLine from './component/timeLine/TimeLine';
-// import PersonalDetails from './component/personalDetailsForm/PersonalDetails';
-import EducationalDetails from './component/EducationalDetailsForm/EducationalDetails';
+import Achievement from './component/Achievements/Achievement';
 
 
 function App() {
@@ -11,8 +10,7 @@ function App() {
     <>
       <Header/>
       <TimeLine/>
-      {/* <PersonalDetails/> */}
-      <EducationalDetails/>
+      <Achievement/>
       <Footer/>
     </>
   )
