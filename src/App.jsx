e6c39818982f681d1dbc97ffa60bc55921c2cae3
JayPalkar <jaypalkar654@gmail.com>
2024-03-16 17:26:55 +0530

    Educational Details form created

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react' // eslint-disable-line no-unused-vars
 import Footer from './component/headerFooter/Footer';
 import Header from './component/headerFooter/Header';
 import TimeLine from './component/timeLine/TimeLine';
-import PersonalDetails from './component/personalDetailsForm/personalDetails';
+// import PersonalDetails from './component/personalDetailsForm/PersonalDetails';
+import EducationalDetails from './component/EducationalDetailsForm/EducationalDetails';
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <>
       <Header/>
       <TimeLine/>
-      <PersonalDetails/>
+      <EducationalDetails/>
       <Footer/>
     </>
   )
--- src/component/EducationalDetailsForm/EducationalDetails.jsx
+++ src/component/EducationalDetailsForm/EducationalDetails.jsx
@@ -5,7 +5,7 @@ function EducationalDetails() {
   return (
     <>
         <div className='educationalDetailForm'>
-            <div className="sscFrom">
+            <div className="From">
                 <h3>SSC</h3>
                 <form className='form'>
                     <div className="row">
@@ -44,19 +44,182 @@ function EducationalDetails() {
                             </div>
                         </div>
                     </div>
+                    <button type="submit" className="btn btn-primary saveBtn">Save</button>
                 </form>
             </div>
-            <div className="sscFrom">
+            <div className="From">
                 <h3>HSC</h3>
+                <form className='form'>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputUniversity" className="form-label">University / Board</label>
+                                <input type="text" className="form-control" id="inputuniversity" placeholder='Enter Name of Your University'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputPassingYear" className="form-label">Month and year of passing</label>
+                                <div className="passingYearMonthDiv">
+
+                                <input type="text" className="form-control passingYNM" id="inputPassingMonth" placeholder='month'/>
+                                <input type="number" className="form-control passingYNM" id="inputPassingYear" placeholder='Year'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputPercentage" className="form-label">Percentage and marks obtain</label>
+                                <input type="number" className="form-control" id="inputPercentage" placeholder='Enter your percentage'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputClass" className="form-label">Class and Division Obtained</label>
+                                <input type="email" className="form-control" id="inputClass" placeholder='Enter Class and Division Obtained'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label className="form-label" htmlFor="customFile">Upload HSC MarkSheet</label>
+                                <input type="file" className="form-control" id="HSC MarkSheet" />
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary saveBtn">Save</button>
+                </form>
             </div>
-            <div className="sscFrom">
+            <div className="From">
                 <h3>Graduate</h3>
+                <form className='form'>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputUniversity" className="form-label">University / Board</label>
+                                <input type="text" className="form-control" id="inputuniversity" placeholder='Enter Name of Your University'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputPassingYear" className="form-label">Month and year of passing</label>
+                                <div className="passingYearMonthDiv">
+
+                                <input type="text" className="form-control passingYNM" id="inputPassingMonth" placeholder='month'/>
+                                <input type="number" className="form-control passingYNM" id="inputPassingYear" placeholder='Year'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputPercentage" className="form-label">Percentage and marks obtain</label>
+                                <input type="number" className="form-control" id="inputPercentage" placeholder='Enter your percentage'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputClass" className="form-label">Class and Division Obtained</label>
+                                <input type="email" className="form-control" id="inputClass" placeholder='Enter Class and Division Obtained'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label className="form-label" htmlFor="customFile">Upload Graduation MarkSheet</label>
+                                <input type="file" className="form-control" id="Graduation MarkSheet" />
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary saveBtn">Save</button>
+                </form>
             </div>
-            <div className="sscFrom">
+            <div className="From">
                 <h3>Post Graduate</h3>
+                <form className='form'>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputUniversity" className="form-label">University / Board</label>
+                                <input type="text" className="form-control" id="inputuniversity" placeholder='Enter Name of Your University'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputPassingYear" className="form-label">Month and year of passing</label>
+                                <div className="passingYearMonthDiv">
+
+                                <input type="text" className="form-control passingYNM" id="inputPassingMonth" placeholder='month'/>
+                                <input type="number" className="form-control passingYNM" id="inputPassingYear" placeholder='Year'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputPercentage" className="form-label">Percentage and marks obtain</label>
+                                <input type="number" className="form-control" id="inputPercentage" placeholder='Enter your percentage'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputClass" className="form-label">Class and Division Obtained</label>
+                                <input type="email" className="form-control" id="inputClass" placeholder='Enter Class and Division Obtained'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label className="form-label" htmlFor="customFile">Upload Post-Graduation MarkSheet</label>
+                                <input type="file" className="form-control" id="Post-Graduation MarkSheet" />
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary saveBtn">Save</button>
+                </form>
             </div>
-            <div className="sscFrom">
+            <div className="From">
                 <h3>PHD</h3>
+                <form className='form'>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputUniversity" className="form-label">University / Board</label>
+                                <input type="text" className="form-control" id="inputuniversity" placeholder='Enter Name of Your University'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputPassingYear" className="form-label">Month and year of passing</label>
+                                <div className="passingYearMonthDiv">
+
+                                <input type="text" className="form-control passingYNM" id="inputPassingMonth" placeholder='month'/>
+                                <input type="number" className="form-control passingYNM" id="inputPassingYear" placeholder='Year'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputPercentage" className="form-label">Percentage and marks obtain</label>
+                                <input type="number" className="form-control" id="inputPercentage" placeholder='Enter your percentage'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputSubject" className="form-label">Subject of Studies</label>
+                                <input type="text" className="form-control" id="inputSubject" placeholder='Enter your Subject of Studies'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="InputClass" className="form-label">Class and Division Obtained</label>
+                                <input type="email" className="form-control" id="inputClass" placeholder='Enter Class and Division Obtained'/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label className="form-label" htmlFor="customFile">Upload PHD MarkSheet</label>
+                                <input type="file" className="form-control" id="PHD MarkSheet" />
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary saveBtn">Save</button>
+                </form>
             </div>
         
         </div> 
