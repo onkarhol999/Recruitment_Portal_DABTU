c16d90ff28a17002201c5eb2ab71497957f6b36e
JayPalkar <jaypalkar654@gmail.com>
2024-03-16 14:16:39 +0530

    Footer update

--- src/component/headerFooter/Footer.jsx
+++ src/component/headerFooter/Footer.jsx
@@ -3,20 +3,22 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <footer className="bg-body-tertiary text-center text-lg-start" id="footerContainer">
-      <div className="container p-4">
-        <a className="footerLinks" href="/"> Terms of Use </a> | 
-        <a className="footerLinks" href="/"> Privacy Statement </a> | 
-        <a className="footerLinks" href="/"> Disclaimer </a> |
-        <a className="footerLinks" href="/"> AICTE </a> |
-        <a className="footerLinks" href="/"> DST </a> |
-        <a className="footerLinks" href="/"> DTE Maharashtra </a> |
-        <a className="footerLinks" href="/"> RUSA </a> |
-        <a className="footerLinks" href="/"> State CET Cell </a>
-      </div>
-      <div className="text-center p-3">
-        © 2024 Copyright:
-        <a className="text-body footerLinks" href="/"> all copyrights reserved</a>
+    <footer className="bg-body-tertiary text-center text-lg-start">
+      <div className="footerContainer">  
+        <div className="container p-4" id="footerContainer">
+          <a className="footerLinks" href="/"> Terms of Use </a> | 
+          <a className="footerLinks" href="/"> Privacy Statement </a> | 
+          <a className="footerLinks" href="/"> Disclaimer </a> |
+          <a className="footerLinks" href="/"> AICTE </a> |
+          <a className="footerLinks" href="/"> DST </a> |
+          <a className="footerLinks" href="/"> DTE Maharashtra </a> |
+          <a className="footerLinks" href="/"> RUSA </a> |
+          <a className="footerLinks" href="/"> State CET Cell </a>
+        </div>
+        <div className="text-center p-3">
+          © 2024 Copyright:
+          <a className="text-body footerLinks" href="/"> all copyrights reserved</a>
+        </div>
       </div>
     </footer>
   )
