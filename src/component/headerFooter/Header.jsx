0439c1791c30623a6298d9d85e8df08545977b11
JayPalkar <jaypalkar654@gmail.com>
2024-03-16 10:22:33 +0530

    Second push for header

--- src/component/headerFooter/Header.jsx
+++ src/component/headerFooter/Header.jsx
@@ -9,10 +9,10 @@ const Header = () => {
         <img className="dbatuLogo" src="https://dbatu.ac.in/wp-content/uploads/2022/12/cropped-OQSD-Wtj.jpeg" alt="College Logo" />
         <div className="topcontainer">
               <div className="Title">
-                <p className="">Dr. Babasaheb Ambedkar Technological University</p> 
-                <p className="">डॉ. बाबासाहेब आंबेडकर तंत्रशास्त्र विद्यापीठ</p>
-                <p className="">Lonere-402103, Tal-Mangaon, Dist-Raigad (M.S.) India</p>
-                <p className="">Recruitment Portal</p>
+                <p>Dr. Babasaheb Ambedkar Technological University</p> 
+                <p>डॉ. बाबासाहेब आंबेडकर तंत्रशास्त्र विद्यापीठ</p>
+                <p>Lonere-402103, Tal-Mangaon, Dist-Raigad (M.S.) India</p>
+                <p>Recruitment Portal</p>
               </div>
         </div>
       </div>
