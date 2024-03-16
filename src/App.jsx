14ec8434691b1de0a53c2d0915ecf9b4ee929959
Onkar hol <holonkar2003@gmail.com>
2024-03-16 16:14:59 +0530

    Progrss bar Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './App.css';
 import Footer from './component/headerFooter/Footer';
 import Header from './component/headerFooter/Header';
 import TimeLine from './component/timeLine/TimeLine';
-// import Home from './component/home/Home';
+import Home from './component/home/Home';
 
 function App() {
   return (
     <>
      <Header/>
     <TimeLine/>
-     {/* <Home/> */}
+     <Home/>
      <Footer/>
     </>
   );
