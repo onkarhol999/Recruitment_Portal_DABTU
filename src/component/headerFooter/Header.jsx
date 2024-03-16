4698ac2471a460d0739c100f8490e696121e8c78
JayPalkar <jaypalkar654@gmail.com>
2024-03-16 10:15:24 +0530

    updated header and color palette

--- src/component/headerFooter/Header.jsx
+++ src/component/headerFooter/Header.jsx
@@ -5,15 +5,15 @@ const Header = () => {
   return (
     <header>
       {/* Top Section */}
-      <div className="bg-white p-4 rounded-md mb-2 d-flex align-items-center justify-content-center">
-        <img className="DBATUlogo" src="https://dbatu.ac.in/wp-content/uploads/2022/12/cropped-OQSD-Wtj.jpeg" alt="College Logo" />
-        <div className="text-center">
-          <div className="text-3xl font-bold">
-            <p className="text-3xl font-bold text-primary">Dr. Babasaheb Ambedkar Technological University</p> 
-            <p className="text-lg mt-1">डॉ. बाबासाहेब आंबेडकर तंत्रशास्त्र विद्यापीठ</p>
-            <p className="text-lg">Lonere-402103, Tal-Mangaon, Dist-Raigad (M.S.) India</p>
-            <p className="text-lg mt-2 text-1xl">Recruitment Portal</p>
-          </div>
+      <div className="headerContainer">
+        <img className="dbatuLogo" src="https://dbatu.ac.in/wp-content/uploads/2022/12/cropped-OQSD-Wtj.jpeg" alt="College Logo" />
+        <div className="topcontainer">
+              <div className="Title">
+                <p className="">Dr. Babasaheb Ambedkar Technological University</p> 
+                <p className="">डॉ. बाबासाहेब आंबेडकर तंत्रशास्त्र विद्यापीठ</p>
+                <p className="">Lonere-402103, Tal-Mangaon, Dist-Raigad (M.S.) India</p>
+                <p className="">Recruitment Portal</p>
+              </div>
         </div>
       </div>
     </header>
