6cbe37002a1d68a1298db9a5384d44e9da207f3e
JayPalkar <jaypalkar654@gmail.com>
2024-03-19 18:42:38 +0530

    Changes made in personalDetaisls and EducationalDetails form

--- src/component/personalDetailsForm/personalDetails.jsx
+++ src/component/personalDetailsForm/personalDetails.jsx
@@ -22,60 +22,66 @@ function PersonalDetails() {
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
-                    <div className="form-group">
-
-                    <label htmlFor="InputName" className="form-label my-0 nameLabel">Full Name</label>
-                    <div className="form-text my-2">(Enter name like: [Last Name] [First Name] [Middle Name])</div>
+                    <label htmlFor="InputName" className="form-label nameLabel">Full Name</label>
+                    <div className="row personalformRow">
+                        <div className="col">
+                            <input type="text" className="form-control" id="inputLasttName" placeholder='Last Name' required/>
+                        </div>
+                        <div className="col">
+                            <input type="text" className="form-control" id="inputFirstName" placeholder='First Name' required/>
+                        </div>
+                        <div className="col">
+                            <input type="text" className="form-control" id="inputMiddleName" placeholder='Middle Name' required/>
+                        </div>
                     </div>
-                    <input type="text" className="form-control" id="inputName" placeholder='Enter Name In given Format'/>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="InputAddress" className="form-label">Current Address</label>
-                    <input type="text" className="form-control" id="inputAddress" placeholder='Enter your address'/>
+                    <input type="text" className="form-control" id="inputAddress" placeholder='Enter your address' required/>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="InputEmail" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="inputEmail" placeholder='Enter your Email'/>
+                    <input type="email" className="form-control" id="inputEmail" placeholder='Enter your Email' required/>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="InputAadhar" className="form-label">Aadhar Card Number</label>
-                    <input type="number" className="form-control" id="inputAadhar" placeholder="Enter your Aadhar Card Number"/>
+                    <input type="text" className="form-control" id="inputAadhar" placeholder="Enter your Aadhar Card Number" maxLength="12" pattern="[0-9]{12}" title="Please enter a valid 12-digit Aadhar card number" required/>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="InputNumber" className="form-label">Mobile Number</label>
-                    <input type="number" className="form-control" id="inputNumber" placeholder="Enter your Mobile Number"/>
+                    <input type="text" className="form-control" maxLength="10" pattern="[0-9]{10}" id="inputNumber" placeholder="Enter your Mobile Number" required/>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="InputDOB" className="form-label">Date of Birth</label>
-                    <input type="date" className="form-control" id="inputDOB" placeholder="Select or enter your Date of birth"/>
+                    <input type="date" className="form-control" id="inputDOB" placeholder="Select or enter your Date of birth" required/>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="InputAge" className="form-label">Age</label>
-                    <input type="number" className="form-control" id="inputAge" placeholder="Enter your age"/>
+                    <input type="number" className="form-control" id="inputAge" placeholder="Enter your age" required/>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="InputNationality" className="form-label">Nationality</label>
-                    <input type="number" className="form-control" id="inputNationality" placeholder="Enter your Nationality"/>
+                    <input type="text" className="form-control" id="inputNationality" placeholder="Enter your Nationality" required/>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
                     <label className="form-label" htmlFor="customFile">Upload Your Nationality Certificate</label>
-                    <input type="file" className="form-control" id="nationalityCertificate" />
+                    <input type="file" className="form-control" id="nationalityCertificate" required />
                 </div>
             </div>
             <div className="col-md-6">
@@ -120,20 +126,29 @@ function PersonalDetails() {
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
-                    <label htmlFor="InputAge" className="form-label">Caste</label>
-                    <select className="form-select" aria-label="Default select example">
-                        <option selected>Select Caste</option>
-                        <option value="SC">SC</option>
-                        <option value="ST">ST</option>
-                        <option value="OBC">OBC</option>
-                        <option value="VJNT">VJ/NT</option>
-                        <option value="SBC">SBC</option>
-                    </select>
+                    <label htmlFor="Inputcategory" className="form-label">Category</label>
+                    <div className="row personalformRow">
+                        <div className="col">
+                            <select className="form-select" aria-label="Default select example">
+                                <option selected>Select Category</option>
+                                <option value="SC">SC</option>
+                                <option value="ST">ST</option>
+                                <option value="OBC">OBC</option>
+                                <option value="VJNT">VJ</option>
+                                <option value="VJNT">NT</option>
+                                <option value="SBC">GC</option>
+                                <option value="SBC">Other</option>
+                            </select>
+                        </div>
+                        <div className="col">
+                            <input type="text" className="form-control" id="inputCaste" placeholder="Enter your Caste"/>
+                        </div>
+                    </div> 
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
-                    <label className="form-label" htmlFor="customFile">Upload Your caste certificate</label>
+                    <label className="form-label" htmlFor="customFile">Upload Your caste certificate / Validity</label>
                     <input type="file" className="form-control" id="casteCertificate" />
                 </div>
             </div>
